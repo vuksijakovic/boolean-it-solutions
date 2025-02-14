01f4ac84adc3887f103b668f1c97a8da80412d4e
vsijakovic1421rn <vsijakovic1421rn@raf.rs>
2025-02-14 23:22:51 +0100

    Migracije za bazu

---
+++ database/migrations/2025_02_14_222045_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+          
+            $table->string('product_number')->primary();
+
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('department_id');
+            $table->unsignedBigInteger('manufacturer_id');
+
+            $table->string('upc')->nullable();
+            $table->string('sku')->nullable();
+            $table->decimal('regular_price', 8, 2)->nullable();
+            $table->decimal('sale_price', 8, 2)->nullable();
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
+
